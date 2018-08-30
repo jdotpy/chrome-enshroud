e34754b92f06cb925ea950ecc88625f94d70e26c
KJ <jdotpy@users.noreply.github.com>
2018-08-30 07:40:19 -0700

    Added auto-reload

--- src/content-script.js
+++ src/content-script.js
@@ -15,11 +15,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 
-const ENSHROUD_TINT_ID = 'e---enshroud-tint';
-
 (function insertTint() {
-  console.log('tinting');
-  tint_ele = document.createElement('div');
+  const ENSHROUD_TINT_ID = 'e---enshroud-tint';
+  const tint_ele = document.createElement('div');
   tint_ele.id = ENSHROUD_TINT_ID;
   tint_ele.style.opactity = 0;
   document.body.appendChild(tint_ele);
@@ -37,6 +35,57 @@ const ENSHROUD_TINT_ID = 'e---enshroud-tint';
   document.addEventListener('keydown', (event) => {
     if (event.keyCode === 66 && event.altKey) {
       cycleTint();
+    }
+  });
+})();
+
+(function makeReload() {
+  console.log('initializing reload');
+  const ENSHROUD_RELOAD_INDICATOR_ID = 'e---enshroud-reload-indicator';
+  const ENSHROUD_RELOAD_KEY = 'enshroud---reload';
+  const RELOAD_INTERVAL = 5000;
+  const reloadFunc = () => window.location.reload(true);
+  let reload_timeout_id = null;
+
+  // Manage DOM element
+  const indicator_ele = document.createElement('div');
+  indicator_ele.appendChild(document.createTextNode('\u21BB'));
+  indicator_ele.id = ENSHROUD_RELOAD_INDICATOR_ID;
+  document.body.appendChild(indicator_ele);
+
+  function cycleReload() {
+    let ele = document.getElementById(ENSHROUD_RELOAD_INDICATOR_ID);
+
+    if (reload_timeout_id === null) {
+      console.log('turning on reload');
+      // Turn on
+      ele.style.display = 'block';
+      reload_timeout_id = setTimeout(reloadFunc, RELOAD_INTERVAL);
+      window.sessionStorage.setItem(ENSHROUD_RELOAD_KEY, 'on');
+    } else {
+      console.log('turning off reload');
+      ele.style.display = 'none';
+      clearTimeout(reload_timeout_id);
+      reload_timeout_id = null;
+      window.sessionStorage.setItem(ENSHROUD_RELOAD_KEY, 'off');
+    }
+
+    const current = parseFloat(ele.style.opacity) || 0;
+    let newOpacity = current + .1;
+    if (newOpacity >= 1) {
+      newOpacity = 0;
+    }
+    ele.style.opacity = newOpacity;
+  }
+
+  // Get state from sessionStorage
+  if (window.sessionStorage.getItem(ENSHROUD_RELOAD_KEY) == 'on') {
+    cycleReload();
+  }
+
+  document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 73 && event.altKey) {
+      cycleReload();
     }
   });
 })();
